f5c1f1b3bcf945aa739580360f494fb27e4ec194
Sophie Huiberts <sophie.huiberts@gmail.com>
2020-08-19 16:25:32 +0200

    fix message editing

--- glue.js
+++ glue.js
@@ -11,19 +11,20 @@ document.head.appendChild(style);
 // There is some leak somewhere that I cannot track down.
 // Instead of resolving it, we make sure not to call on KaTeX too often.
 var LaTeXmessages = function(){
-      var messages = document.getElementsByTagName("div");
-      for(var i = 0; i < messages.length; i++)
-      {
-            if( messages.item(i).triedlatex != true)
-            {
-                 renderMathInElement(messages.item(i));
-                 messages.item(i).triedlatex = true
-            }
-      }
+    var messages = document.getElementsByTagName("div");
+    for(var i = 0; i < messages.length; i++)
+    {
+        if( messages.item(i).triedlatex != true && messages.item(i).className.match("messageContent") != null)
+        {
+            renderMathInElement(messages.item(i));
+            messages.item(i).triedlatex = true
+        }
+    }
 }
 
 window.setInterval(function(){
 	 LaTeXmessages();
+    console.log('bleh');
 }, 400);
 
 
